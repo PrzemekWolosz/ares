f190086f7d556a5a76225f1f29fade4f103f8a2f
PrzemekWolosz <przemyslaw.wolosz1@gmail.com>
2020-10-18 10:09:58 +0200

    Add package Errors

---
+++ src/main/java/com/przemyslawwolosz/core/error/ErrorResponse.java
@@ -0,0 +1,16 @@
+package com.przemyslawwolosz.core.error;
+
+import java.util.List;
+
+public class ErrorResponse {
+
+    private List<String> errors;
+
+    public ErrorResponse(List<String> errors) {
+        this.errors = errors;
+    }
+
+    public List<String> getErrors() {
+        return errors;
+    }
+}
